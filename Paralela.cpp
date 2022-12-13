d81cefb6cfe9fb046c327294226cc63648482a6f
Ráfaga dos Santos Trentini <rafaga.trentini@gmail.com>
2022-12-13 16:50:14 -0300

    Add files via upload

---
+++ Paralela.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <omp.h>
+#include <time.h>
+
+clock_t tInicio, tFim, tDecorrido;
+using std::cout; using std::string; using std::endl;
+
+string readFile(const string& path) {
+    struct stat sb{};
+    string res;
+
+    FILE* file = fopen(path.c_str(), "r");
+ 
+    stat(path.c_str(), &sb);
+    res.resize(sb.st_size);
+    fread(const_cast<char*>(res.data()), sb.st_size, 1, file);
+    fclose(file);
+
+    return res;
+}
+
+
+int main() {
+    string filename("arquivos/DNA.txt");
+    string content = readFile(filename);
+    
+	char fita[content.length()];
+
+	strcpy(fita,content.c_str());
+
+	tInicio = clock();
+	omp_set_num_threads(2);
+	#pragma omp parallel for
+	for (int i = 0; i < content.length(); i++) {	
+		printf("Thread %d\n", omp_get_thread_num());		
+		switch (toupper(fita[i])) {
+			case 'A':
+				fita[i] = 'T';
+				break;
+			case 'C':
+				fita[i] = 'G';
+				break;
+			case 'T':
+				fita[i] = 'A';
+				break;
+			case 'G':
+				fita[i] = 'C';
+				break;
+		}
+	}
+
+	tFim = clock();
+	tDecorrido = 0;
+	tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+
+	printf("Tempo decorrido: %d\n", tDecorrido);
+
+}
